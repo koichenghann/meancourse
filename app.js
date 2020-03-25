8c907e0e2bfe059b5aa665fbe268d84c8f19bb64
ChengHannKoi <chengghannkoi@gmail.com>
2020-03-25 19:26:00 +0800

    included connection to all controllers and route in app

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 
 const postsRoutes = require("./routes/posts");
 const userRoutes = require("./routes/user");
+const materialRoutes = require("./routes/material");
+const submissionRoute = require("./routes/submission");
 
 const app = express();
 
@@ -38,5 +40,7 @@ app.use((req, res, next) => {
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
- 
+app.use("/api/material", materialRoutes);
+app.use("/api/submission", submissionRoute);
+
 module.exports = app;
