a209358903abdb448c2849a7e057eb5a2fa4f323
ChengHannKoi <chengghannkoi@gmail.com>
2020-03-25 17:50:19 +0800

    Revert "add component from esweb for compatibility testing"
    This reverts commit 9a7fb037e330173ca8425beaddc5ec4917d5c16e.

--- models/collectorMaterial.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const collectorMaterialSchema = mongoose.Schema({
-    collectorID: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
-    materialID: { type: mongoose.Schema.Types.ObjectId, ref: "Material", required: true}
-});
-
-module.exports = mongoose.model('collectorMaterial', collectorMaterialSchema);
